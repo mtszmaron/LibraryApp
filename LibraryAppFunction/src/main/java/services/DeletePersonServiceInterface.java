2bbd218f83cffa507e2407b7a2435c463c590e2b
Wapa <72733062+mtszmaron@users.noreply.github.com>
2023-06-05 15:19:26 +0200

    Update

--- LibraryAppFunction/src/main/java/services/DeletePersonServiceInterface.java
+++
@@ -1,8 +0,0 @@
-package services;
-
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
-
-public interface DeletePersonServiceInterface {
-    public default void deletePerson(APIGatewayProxyRequestEvent input, Context context) { }
-}
